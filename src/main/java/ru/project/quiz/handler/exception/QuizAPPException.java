1f42d40acda649d9f4a814b2c00e1aaa8a915306
Sergey Sokolovskii <sergunok1242@gmail.com>
2021-04-20 21:50:39 +0300

    ISSUE#5 зачистка эксепшенов Добавлен один общий

---
+++ src/main/java/ru/project/quiz/handler/exception/QuizAPPException.java
@@ -0,0 +1,7 @@
+package ru.project.quiz.handler.exception;
+
+public class QuizAPPException extends RuntimeException{
+    public QuizAPPException(String message) {
+        super(message);
+    }
+}
