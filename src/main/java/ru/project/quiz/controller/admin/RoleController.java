b73124133e5f215d01c47fc33416dca86d8e7dca
Fr0z3Nn <luckyjohn057@gmail.com>
2021-04-20 21:00:13 +0300

    ISSUE#29 Добавлено: Метод поиска по имени в RoleController

--- src/main/java/ru/project/quiz/controller/admin/RoleController.java
+++ src/main/java/ru/project/quiz/controller/admin/RoleController.java
@@ -3,23 +3,28 @@ package ru.project.quiz.controller.admin;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.project.quiz.domain.dto.ituser.ITUserDTO;
+import ru.project.quiz.domain.dto.ituser.RoleDTO;
+import ru.project.quiz.domain.entity.ituser.Role;
 import ru.project.quiz.domain.enums.ituser.PermissionType;
+import ru.project.quiz.mapper.ituser.RoleMapper;
 import ru.project.quiz.service.ituser.RoleService;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/roles")
 @Tag(name = "Контроллер ролей")
 public class RoleController {
     private final RoleService roleService;
-
-    public RoleController(RoleService roleService) {
-        this.roleService = roleService;
-    }
+    private final RoleMapper roleMapper;
 
     @Operation(summary = "Создать новую роль", security = @SecurityRequirement(name = "bearerAuth"))
     @PostMapping
@@ -33,9 +38,20 @@ public class RoleController {
         roleService.deleteRole(name);
     }
 
+    //TODO ISSUE#36
     @Operation(summary = "Лист пользователей с данной ролью", security = @SecurityRequirement(name = "bearerAuth"))
-    @GetMapping("/{roleName}")
+    @GetMapping("/list/{roleName}")
     public List<ITUserDTO> findUsersByRole(@PathVariable String roleName) {
         return roleService.findUsersByRole(roleName);
     }
+
+    @Operation(summary = "Получить роль по ID", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/{role-name}")
+    public ResponseEntity<RoleDTO> findRoleByID(@PathVariable("role-name") String name) {
+        Optional<Role> optRoleById = roleService.findRoleByName(name);
+        return optRoleById
+                .map(role -> new ResponseEntity<>(roleMapper.roleDTOFromRole(role), HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
+
 }
--- src/main/java/ru/project/quiz/service/ituser/RoleService.java
+++ src/main/java/ru/project/quiz/service/ituser/RoleService.java
@@ -1,9 +1,11 @@
 package ru.project.quiz.service.ituser;
 
 import ru.project.quiz.domain.dto.ituser.ITUserDTO;
+import ru.project.quiz.domain.entity.ituser.Role;
 import ru.project.quiz.domain.enums.ituser.PermissionType;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoleService {
     void addNewRole(String name, PermissionType permissionType);
@@ -11,4 +13,6 @@ public interface RoleService {
     void deleteRole(String name);
 
     List<ITUserDTO> findUsersByRole(String name);
+
+    Optional<Role> findRoleByName(String name);
 }
