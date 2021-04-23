118889d70c13d3b8da8b38605f56acac7adc0dc8
Fr0z3Nn <luckyjohn057@gmail.com>
2021-04-23 19:48:45 +0300

    HOT FIX Исправлено: Новая роль создает из ДТО, а не из параметров

--- src/main/java/ru/project/quiz/service/ituser/RoleService.java
+++ src/main/java/ru/project/quiz/service/ituser/RoleService.java
@@ -1,14 +1,14 @@
 package ru.project.quiz.service.ituser;
 
 import ru.project.quiz.domain.dto.ituser.ITUserDTO;
+import ru.project.quiz.domain.dto.ituser.RoleDTO;
 import ru.project.quiz.domain.entity.ituser.Role;
-import ru.project.quiz.domain.enums.ituser.PermissionType;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RoleService {
-    void addNewRole(String name, PermissionType permissionType);
+    void addNewRole(Role role);
 
     void deleteRole(String name);
 
