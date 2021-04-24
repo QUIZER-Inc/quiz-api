dafeeb4c7d9ce850c13694f25118bb2e13f95c57
Fr0z3Nn <luckyjohn057@gmail.com>
2021-04-25 00:20:05 +0300

    Исправлено: RoleDTO исправлен

--- src/main/java/ru/project/quiz/domain/dto/ituser/RoleDTO.java
+++ src/main/java/ru/project/quiz/domain/dto/ituser/RoleDTO.java
@@ -1,6 +1,5 @@
 package ru.project.quiz.domain.dto.ituser;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,16 +12,8 @@ import java.util.Set;
 @AllArgsConstructor
 public class RoleDTO {
 
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-    private long id;
-
     private String name;
 
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Set<PermissionType> permissions;
 
-    public RoleDTO(String name, Set<PermissionType> permissions) {
-        this.name = name;
-        this.permissions = permissions;
-    }
 }
